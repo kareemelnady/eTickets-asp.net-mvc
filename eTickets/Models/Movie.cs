7f49478fa4758fd748af335ab3bd47e53c148392
kareem <kareemnady6123@gmail.com>
2023-06-12 22:26:17 +0300

    firstMigration

--- eTickets/Models/Movie.cs
+++ eTickets/Models/Movie.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,5 +21,17 @@ namespace eTickets.Models
         public DateTime EndDate { get; set; }
         public MovieCategory MovieCategory { get; set; }
 
+        //Relationships
+        public List<Actor_Movie> Actors_Movies { get; set; }
+
+        //cinema
+        public int CinemaId { get; set; }
+        [ForeignKey("CinemaId")]
+        public Cinema Cinema { get; set; }
+
+        //Producer
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+        public Producer Producer { get; set; }
     }
 }
